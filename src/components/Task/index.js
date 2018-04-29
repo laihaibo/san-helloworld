bb285a5f40aaca252aa97479f8f54a4f2698db35
laihaibo <2466006555@qq.com>
2018-04-29 09:42:31 +0800

    update task3 new

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -4,22 +4,34 @@ import styles from './index.css';
 
 class Task extends Component {
   static template = `<div class="{{styles.Task}}">
-  Task
+  <div id="blockC" class="{{isBlue ? styles.BlockBlue : styles.Block}}"  on-click="onClickClass" />
+  <div id="blockS" class="{{styles.Block}}" style="{{blockS}}" on-click="onClickStyle" />
   </div>`;
 
   static dataTypes = {
-    items: DataTypes.array,
+    styles: DataTypes.object,
   };
 
   initData() {
     return {
       styles,
+      isBlue: false,
+      blockS: '',
     };
   }
 
   static computed = {};
 
   created() {}
+
+  onClickClass() {
+    let isBlue = this.data.get('isBlue');
+    this.data.set('isBlue', !isBlue);
+  }
+
+  onClickStyle() {
+    this.data.set('blockS', 'background: yellow');
+  }
 }
 
 export default Task;
