be6b07ad94dff4a2025fb2e876580c3a3acb1836
laihaibo <2466006555@qq.com>
2018-05-09 18:08:15 +0800

    init task2.7.1 base

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -4,22 +4,35 @@ import styles from './index.css';
 
 class Task extends Component {
   static template = `<div class="{{styles.Task}}">
-  Task
+  <div>
+    <button on-click="toggle">TOGGLE</button>
+    <span s-if="show" s-transition="hook">
+      Transition Layer
+    </span>
+  </div>
   </div>`;
 
   static dataTypes = {
-    items: DataTypes.array,
+    show: DataTypes.bool
   };
 
   initData() {
     return {
       styles,
+      show: true
     };
   }
 
   static computed = {};
 
   created() {}
+
+  toggle() {
+    console.log(this.data.get('show'));
+    this.data.set('show', !this.data.get('show'));
+  }
+
+  hook = {};
 }
 
 export default Task;
